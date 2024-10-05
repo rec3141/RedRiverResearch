00b8424553193148341e37776d28e8db47534f5a
R. Eric Collins <rec3141@gmail.com>
2024-10-05 11:26:46 -0500

    Update and rename birdbox-remotesetup.sh to BirdNet-Pi/birdbox-remotesetup.sh

--- BirdNet-Pi/birdbox-remotesetup.sh
+++ BirdNet-Pi/birdbox-remotesetup.sh
@@ -1,19 +1,42 @@
 sudo apt-get install dnsutils uuid-runtime tmux
+
 tmux
+
 cd
-domainname=redriverresearch.ca
-curl -o dynamicdns.bash https://raw.githubusercontent.com/clempaul/dreamhost-dynamic-dns/refs/heads/master/dynamicdns.bash
-chmod +x dynamicdns.bash
-echo "@hourly ~/dynamicdns.bash -S -v -k {DREAMHOST_API_KEY} -r $(hostname).${domainname}" > ~/.config/crontab.txt
-chmod +x ~/.config/crontab.txt
-crontab ~/.config/crontab.txt
-sed -i "s/BIRDNETPI_URL=$/BIRDNETPI_URL=$(hostname).${domainname}/" ~/BirdNET-Pi/birdnet.conf
-ipaddress=192.168.0.161
+
 macaddress=$(nmcli device show wlan0 | grep GENERAL.HWADDR | tr -s ' ' | cut -f2 -d' ')
+
 echo $macaddress #WRITE THIS DOWN, YOU'LL NEED IT LATER
+
+ipaddress=192.168.0.161
+
+domainname=redriverresearch.ca
+
+sed -i "s/BIRDNETPI_URL=$/BIRDNETPI_URL=$(hostname).${domainname}/" ~/BirdNET-Pi/birdnet.conf
+
+curl -o dynamicdns.bash https://raw.githubusercontent.com/clempaul/dreamhost-dynamic-dns/refs/heads/master/dynamicdns.bash
+
+chmod +x dynamicdns.bash
+
+echo "@hourly ~/dynamicdns.bash -S -v -k {DREAMHOST_API_KEY} -r $(hostname).${domainname}" > ~/.config/crontab.txt
+
+chmod +x ~/.config/crontab.txt
+
+crontab ~/.config/crontab.txt
+
+echo "
+${hostname}.${domainname} {
+reverse_proxy $ipaddress:80
+}" | sudo tee -a /etc/caddy/Caddyfile
+
 wifi=$(nmcli device show wlan0 | grep GENERAL.CONNECTION | tr -s ' ' | cut -f2 -d' ')
+
 sudo nmcli connection modify $wifi ipv4.addresses ${ipaddress}/24
+
 sudo nmcli connection modify $wifi ipv4.gateway 192.168.0.1
+
 sudo nmcli connection modify $wifi ipv4.dns "8.8.8.8 8.8.4.4"
+
 sudo nmcli connection modify $wifi ipv4.method manual
+
 sudo nmcli connection down $wifi; sudo nmcli connection up $wifi
