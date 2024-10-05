e4724a038d6e1c7fb1a6c6877e01775d00849eaf
R. Eric Collins <rec3141@gmail.com>
2024-10-05 11:46:22 -0500

    Update birdbox-remotesetup.sh

--- BirdNet-Pi/birdbox-remotesetup.sh
+++ BirdNet-Pi/birdbox-remotesetup.sh
@@ -6,23 +6,27 @@ tmux new-session -d -s mysession "bash $0 inside_tmux"  # Pass the script as an 
 if [ "$1" == "inside_tmux" ]; then
 # The rest of the script will run inside the tmux session
 
-#install necessary packages and set up variables
+
+####install necessary packages and set up variables
 
 cd
 
 sudo apt-get install dnsutils uuid-runtime tmux
 
-macaddress=$(nmcli device show wlan0 | grep GENERAL.HWADDR | tr -s ' ' | cut -f2 -d' ')
+macaddress=$(nmcli device show wlan0 | grep GENERAL.HWADDR | awk '{print $2}')
 
-echo "MAC ADDRESS (WRITE THIS DOWN) $macaddress"
+echo "MAC ADDRESS (WRITE THIS DOWN): $macaddress"
 
-ipaddress=192.168.0.161
+ipaddress=$(nmcli device show wlan0 | grep IP4.ADDRESS | awk '{print $2}' | cut -f1 -d'/')
+
+echo "IP ADDRESS (WRITE THIS DOWN): $ipaddress"
 
 domainname=redriverresearch.ca
 
-# this sets up the bird box to request a static ip address
 
-wifi=$(nmcli device show wlan0 | grep GENERAL.CONNECTION | tr -s ' ' | cut -f2 -d' ')
+#### this sets up the bird box to request a static ip address
+
+wifi=$(nmcli device show wlan0 | grep GENERAL.CONNECTION | awk '{print $2}')
 
 sudo nmcli connection modify $wifi ipv4.addresses ${ipaddress}/24
 
@@ -36,13 +40,16 @@ sudo nmcli connection down $wifi
 
 sudo nmcli connection up $wifi
 
-# this sets up the bird box to host its website on the redriverresearch.ca domain
+
+#### this sets up the bird box to host its website on the redriverresearch.ca domain
 
 sed -i "s/BIRDNETPI_URL=$/BIRDNETPI_URL=$(hostname).${domainname}/" ~/BirdNET-Pi/birdnet.conf
 
 curl -o dynamicdns.bash https://raw.githubusercontent.com/clempaul/dreamhost-dynamic-dns/refs/heads/master/dynamicdns.bash
 
 chmod +x dynamicdns.bash
+
+~/dynamicdns.bash -S -v -k ${DREAMHOST_API_KEY} -r $(hostname).${domainname}
 
 echo "@hourly ~/dynamicdns.bash -S -v -k ${DREAMHOST_API_KEY} -r $(hostname).${domainname}" > ~/.config/crontab.txt
 
